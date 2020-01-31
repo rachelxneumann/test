28d21fdbc9adb6eafefe9f395934d45d2b0de090
rachelxneumann <41024651+rachelxneumann@users.noreply.github.com>
2020-01-31 13:54:45 -0800

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -12,7 +12,7 @@ import java.awt.Color;
 public class calculator extends menuControl {
 
 	/**
-	 * 
+	Hello commenting here * 
 	 */
 	// view section ++
 	private static final long serialVersionUID = 1L;
